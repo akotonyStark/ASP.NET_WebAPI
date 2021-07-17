a66eb491a3be2b7e9c113dc1016b68658e4163db
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-07-17 22:52:15 +0000

    quotes controller

---
+++ QuotationsAPI/Controllers/QuotesController.cs
@@ -0,0 +1,26 @@
+﻿using QuotationsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace QuotationsAPI.Controllers
+{
+    public class QuotesController: ApiController
+    {
+        List<Quote> _quotes = new List<Quote>()
+        {
+             new Quote() {Id=0, Author="Spiderman", Description = "With great power comes great responsibility", Title = "Power" }
+
+        };
+
+
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+
+       
+    }
+}
