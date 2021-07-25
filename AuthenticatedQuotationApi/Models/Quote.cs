9a970889a25163aee49b8736b8826409a11e9716
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-07-25 13:09:09 +0000

    Create Quote.cs

---
+++ AuthenticatedQuotationApi/Models/Quote.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticatedQuotationApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(25), MinLength(3), MaxLength(25)]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Type { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
