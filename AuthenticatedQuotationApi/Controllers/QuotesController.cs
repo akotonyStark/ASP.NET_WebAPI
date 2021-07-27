b1075a30abb508b91e1cac44803cbdb8308a1978
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-07-27 21:32:10 +0000

    userID added

--- AuthenticatedQuotationApi/Controllers/QuotesController.cs
+++ AuthenticatedQuotationApi/Controllers/QuotesController.cs
@@ -1,5 +1,5 @@
 using AuthenticatedQuotationApi.Models;
-
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -78,6 +78,10 @@ namespace AuthenticatedQuotationApi.Controllers
         [HttpPost]
         public IHttpActionResult PostQuotes([FromBody] Quote quote)
         {
+
+            string userId = User.Identity.GetUserId();
+            quote.UserId = userId;
+             
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
